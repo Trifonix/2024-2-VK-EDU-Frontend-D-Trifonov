b70c105e4779868c018af4287d3fdb38df601d52
Trifonix <trifonix@rocketmail.com>
2024-09-26 21:45:51 +0300

    negative cases in bottom of tests

--- convertBytesToHuman.test.js
+++ convertBytesToHuman.test.js
@@ -9,14 +9,6 @@
 
 import convertBytesToHuman from './convertBytesToHuman';
 
-test('Возвращает false для неправильного типа данных', () => {
-  expect(convertBytesToHuman(-1)).toBe(false);
-  expect(convertBytesToHuman('string')).toBe(false);
-  expect(convertBytesToHuman(null)).toBe(false);
-  expect(convertBytesToHuman(undefined)).toBe(false);
-  expect(convertBytesToHuman(NaN)).toBe(false);
-});
-
 test('Возвращает корректное значение для чисел', () => {
   expect(convertBytesToHuman(0)).toBe('0 B');
   expect(convertBytesToHuman(5)).toBe('5 B');
@@ -41,4 +33,12 @@ test('Округляет значение до двух знаков после 
 test('Работает для больших чисел', () => {
   expect(convertBytesToHuman(1125899906842624)).toBe('1 PB');
   expect(convertBytesToHuman(1152921504606846976)).toBe('1 EB');
+});
+
+test('Возвращает false для неправильного типа данных', () => {
+  expect(convertBytesToHuman(-1)).toBe(false);
+  expect(convertBytesToHuman('string')).toBe(false);
+  expect(convertBytesToHuman(null)).toBe(false);
+  expect(convertBytesToHuman(undefined)).toBe(false);
+  expect(convertBytesToHuman(NaN)).toBe(false);
 });
