ae891d66a12ea09a075f000b23d58f99b401c41f
Trifonix <trifonix@rocketmail.com>
2024-09-26 21:44:05 +0300

    add good name for variables in convertBytesToHuman.js

--- convertBytesToHuman.js
+++ convertBytesToHuman.js
@@ -21,11 +21,11 @@ export default function convertBytesToHuman(bytes) {
   if (typeof bytes !== 'number' || bytes < 0 || isNaN(bytes)) {
     return false;
   }
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'];
+  const sizeUnits = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'];
   if (bytes === 0) {
     return '0 B';
   }
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  const value = bytes / Math.pow(1024, i);
-  return `${parseFloat(value.toFixed(2))} ${sizes[i]}`;
+  const unitIndex = Math.floor(Math.log(bytes) / Math.log(1024));
+  const convertedValue = bytes / Math.pow(1024, unitIndex);
+  return `${parseFloat(convertedValue.toFixed(2))} ${sizeUnits[unitIndex]}`;
 }
